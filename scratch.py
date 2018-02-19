a435b209a32a5041bd0c2256786a6476aa1411c1
Shirshak Sharma <shirshak.sharma@gmail.com>
2018-02-19 18:43:28 -0500

    added code to draw the outside corners?

--- scratch.py
+++ scratch.py
@@ -6,7 +6,7 @@ import glob
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((7*7, 3), np.float32)
+objp = np.zeros((7 * 7, 3), np.float32)
 objp[:, :2] = np.mgrid[0:7, 0:7].T.reshape(-1, 2)
 
 # Arrays to store object points and image points from all the images.
@@ -35,14 +35,28 @@ while True:
         # img = cv2.drawChessboardCorners(img, (7, 7), corners, ret)
         # img2 = cv2.rectangle(images,)
 
-        img2 = cv2.line(img2, (corners2[0][0][0], corners2[0][0][1]),
-                        (corners2[6][0][0], corners2[6][0][1]), (0, 255, 0), 5)
-        img2 = cv2.line(img2, (corners2[6][0][0], corners2[6][0][1]),
-                        (corners2[48][0][0], corners2[48][0][1]), (0, 255, 0), 5)
-        img2 = cv2.line(img2, (corners2[48][0][0], corners2[48][0][1]),
-                        (corners2[42][0][0], corners2[42][0][1]), (0, 255, 0), 5)
-        img2 = cv2.line(img2, (corners2[0][0][0], corners2[0][0][1]),
-                        (corners2[42][0][0], corners2[42][0][1]), (0, 255, 0), 5)
+        corner1 = (corners[0][0][0] - abs(corners[0][0][0] - corners[1][0][0]),
+                   corners[0][0][1] - abs(corners[0][0][1] - corners[7][0][1]))
+        corner2 = (corners[6][0][0] + abs(corners[6][0][0] - corners[5][0][0]),
+                   corners[6][0][1] - abs(corners[6][0][1] - corners[13][0][1]))
+        corner3 = (corners[42][0][0] - abs(corners[42][0][0] - corners[43][0][0]),
+                   corners[42][0][1] + abs(corners[42][0][1] - corners[35][0][1]))
+        corner4 = (corners[48][0][0] + abs(corners[48][0][0] - corners[47][0][0]),
+                   corners[48][0][1] + abs(corners[48][0][1] - corners[41][0][1]))
+
+        img2 = cv2.line(img2, corner1, corner2, (255, 0, 0), 5)
+        img2 = cv2.line(img2, corner1, corner3, (255, 0, 0), 5)
+        img2 = cv2.line(img2, corner4, corner2, (255, 0, 0), 5)
+        img2 = cv2.line(img2, corner4, corner3, (255, 0, 0), 5)
+
+        img2 = cv2.line(img2, (corners[0][0][0], corners[0][0][1]),
+                        (corners[6][0][0], corners[6][0][1]), (0, 255, 0), 5)
+        img2 = cv2.line(img2, (corners[6][0][0], corners[6][0][1]),
+                        (corners[48][0][0], corners[48][0][1]), (0, 255, 0), 5)
+        img2 = cv2.line(img2, (corners[48][0][0], corners[48][0][1]),
+                        (corners[42][0][0], corners[42][0][1]), (0, 255, 0), 5)
+        img2 = cv2.line(img2, (corners[0][0][0], corners[0][0][1]),
+                        (corners[42][0][0], corners[42][0][1]), (0, 255, 0), 5)
 
         cv2.imshow('img', img2)
     if cv2.waitKey(1) & 0xFF == ord('q'):
