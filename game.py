da622368940135ecfdf0bb9b038ac76d5d1ac3cd
tenhagena <tenhagena@wit.edu>
2018-03-29 13:23:44 -0400

    fixed exit of game

--- game.py
+++ game.py
@@ -7,11 +7,6 @@ from board import get_board, which_square
 board = {}
 filled = ''
 
-images = cv2.VideoCapture(0)
-cv2.namedWindow("img")
-cv2.setMouseCallback("img", click)
-retever = False
-
 
 def click(event, x, y, flags, param):
     global filled
@@ -21,15 +16,25 @@ def click(event, x, y, flags, param):
             filled = which_square(board, (x, y))
 
 
+images = cv2.VideoCapture(0)
+cv2.namedWindow("img")
+cv2.setMouseCallback("img", click)
+retever = False
+
+
 while True:
     key = cv2.waitKey(1) & 0xFF
     sta, img = images.read()
     img2 = img
+    # Scans the curent image for a board
     if key == ord('s'):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         board, retever = get_board(gray)
+    # Exits the game
     if key == ord('q'):
         break
+
+    # DRAW PHASE
     if retever:
         for key, square in board.items():
             pts = np.array([square['TL'], square['TR'],
