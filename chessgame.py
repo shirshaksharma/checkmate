18bd0c33cba312d312cd6429db67a9da0ea85ac5
tenhagena <tenhagena@wit.edu>
2018-04-12 13:33:09 -0400

    Added direct path to the board write

--- chessgame.py
+++ chessgame.py
@@ -1,15 +1,20 @@
 import chess
 import chess.svg
+import os
 
 
 class chessGame():
     global board
     global squares
+    global path
 
     def __init__(self):
         global board
-        board = chess.Board()
         global squares
+        global path
+
+        path = os.path.dirname(__file__)
+        board = chess.Board()
         squares = ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'b1',
                    'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'c1', 'c2',
                    'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'd1', 'd2', 'd3',
@@ -22,7 +27,7 @@ class chessGame():
 
     def show(self):
         img = chess.svg.board(board=board)
-        with open("board.svg", 'w') as file:
+        with open(path + "/board.svg", 'w') as file:
             for line in img:
                 file.write(line)
 
